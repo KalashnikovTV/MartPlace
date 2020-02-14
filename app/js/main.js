583accf4f8c0960034024b860db9bedc55c7faef
KalashnikovTV <max.malaschkov@mail.ru>
2020-02-15 00:29:30 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "13px",
+        readOnly: true,
+        normalFill: "#ddd",
+        multiColor: {
+            "startColor": "#ffc000", //RED
+            "endColor"  : "#ffc000"  //GREEN
+        }
+    });
+
+    $('.products-slider__inner').slick({
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+    });
+
     var mixer = mixitup('.products__inner-box');
     
 });
